201a1ce63ce264011213c3d24b1b565162546f17
Sbonelo01 <sbonelomkhize15@gmail.com>
2025-11-25 17:19:45 +0200

    removed mobile app folder

--- mobile-app/jest.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  testMatch: ['**/__tests__/**/*.test.(ts|tsx|js)'],
-};
